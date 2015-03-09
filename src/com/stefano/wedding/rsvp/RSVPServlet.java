f2b758044bffe3be2610b5cfbdd59f24b72b9c79
Stefano Franz <stefano.franz@byhiras.com>
2015-03-09 15:27:52 +0000

    Add some words - make it more gray

---
+++ src/com/stefano/wedding/rsvp/RSVPServlet.java
@@ -0,0 +1,18 @@
+package com.stefano.wedding.rsvp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Author stefanofranz
+ */
+public class RSVPServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+
+}
